495a84aef8e0251586ab5bc72286c04a4c9fe446
Jader Moura <jader.moura.gg@gmail.com>
2026-06-07 17:13:51 -0300

    vercel speed insight

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata, Viewport } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import QueryProvider from "@/components/QueryProvider";
 
 const geistSans = Geist({
@@ -113,6 +114,7 @@ export default function RootLayout({
         />
         <QueryProvider>{children}</QueryProvider>
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
